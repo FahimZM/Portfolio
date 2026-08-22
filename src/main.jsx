9a7f5bd379be83a86a93dfa1928d4499f4e27cc9
Fahim Zaman <fahim.zaman110@gmail.com>
2026-08-21 20:31:37 -0700

    removed test file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import App_test from './App_test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
